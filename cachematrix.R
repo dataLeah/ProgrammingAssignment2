d617a34ffda988357a32cac9262aff53096e7d5c
LiyaM <lm6g15@163.com>
2018-09-20 23:45:22 +0800

    Leah's 2nd assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#create a series of fucntion to£º
+#1.set the matrix
+#2.get the matrix
+#3.set the inverse input matrix 
+#4.get the inverse of the input matrix
+makeCacheMatrix<-function(x=matrix()){
+  mt<-NULL
+  set<-function(y){
+    x<<-y
+    mt<<-NULL
+  }
+  get<-function()x
+  setSolve<-function(m_trans)mt<<-m_trans  #if the m_trans  has already existed
+  getSolve<-function()mt
+  list(set=set,get=get,
+       setSolve=setSolve,getSolve=getSolve)
+}
+##cache the inverse of the input matrix if the inverse has already existed, otherwise
+##calculate the inverse.
+cacheSolve<-function(x,...){ #x here is the list of the get inverse functions
+  mt<-x$getSolve()
+  if(!is.null(mt)){
+    message("getting cached solution of tranferring the matrix")
+    return(mt)
+  }
+  m<-x$get()
+  mt<-solve(m,...)
+  x$setSolve(mt)
+  x$getSolve()  ## get the inverse of the "x"
+  mt            ## Return a matrix that is the inverse of 'x'
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
